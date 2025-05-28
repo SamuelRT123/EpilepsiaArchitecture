02a0a7b7a5d1ca0723fa38c0e85f0f91a2dfc062
samuelrt123 <se.rodriguez2@uniandes.edu.co>
2025-05-28 18:45:16 -0500

    Ups

--- examenes/examenes/urls.py
+++ examenes/examenes/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, url
+from django.urls import include, path
+from django.conf.urls import url, include
 
 from manejador_examenes import views
 
