c43122da27a0d928f1cb10926963b6bcc4168773
samuelrt123 <se.rodriguez2@uniandes.edu.co>
2025-05-28 17:51:12 -0500

    Actualizacion depliegues

--- usuarios/usuarios/settings.py
+++ usuarios/usuarios/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qvi_8e@*1j==%=2vs8w+4wecauvy395t4*%8bw+5-w*cpjau&8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
